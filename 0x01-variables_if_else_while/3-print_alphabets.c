391bec954332af3be1a772f81421258ef978b6a1
gusgalvism <gusgalvism@gmail.com>
2020-02-06 21:09:26 +0000

    FIX 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,21 +9,18 @@
  */
 int main(void)
 {
-	char alphabet = 'a';
-
-    	while (alphabet <= 'z')
-	{
-		putchar(alphabet);
-		alphabet++;
-	}
-    
-    	alphabet = 'A';
-
-    	while (alphabet <= 'Z')
-	{
-		putchar(alphabet);
-		alphabet++;
-	}
-	putchar('\n');
-	return (0);
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+alphabet = 'A';
+while (alphabet <= 'Z')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
 }
