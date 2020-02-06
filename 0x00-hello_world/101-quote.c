b80621c2b2356a479bfe5d20bb8930fa964f76cf
gusgalvism <gusgalvism@gmail.com>
2020-02-06 02:16:50 +0000

    FIX3 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
-
+#include "stdio.h"
+#include "string.h"
+#include "unistd.h"
 /**
- * main - Entry point
- *
- * Return: Always 1 (Error)
+ * main - main block
+ * Return: 1
  */
 int main(void)
 {
-char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-perror(s);
+int len;
+len = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
 return (1);
 }
