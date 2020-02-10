ca48894e1c5fdfe07de51789093cc30713e67f61
gusgalvism <gusgalvism@gmail.com>
2020-02-10 22:47:05 +0000

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+
+	if (n > '0')
+
+		return (1);
+
+	_putchar ('+')
+	else if (n == '0')
+
+		return (0);
+
+	_putchar ('0')
+	else
+
+		return (-1);
+
+	_putchar ('-')
+
+}
