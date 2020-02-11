ce9b6074644c2705d7a15ad324da6a1b3f2473c4
gusgalvism <gusgalvism@gmail.com>
2020-02-11 16:05:56 +0000

    FIX6 3,4,5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * int _isalpha - check the code for Holberton School students.
+ * _isalpha - check the code for Holberton School students.
  * @c: parameter
  * Return: Always 0.
  */
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 
 /**
- * int print_sign - check the code for Holberton School students.
+ * print_sign - check the code for Holberton School students.
  * @n: parameter
  * Return: Always 0.
  */
 
 int print_sign(int n)
 {
-	if (n > '0')
+	int n = 0;
 
+	if (n > '0')
 	{
 		_putchar ('+')
 			return (1);
