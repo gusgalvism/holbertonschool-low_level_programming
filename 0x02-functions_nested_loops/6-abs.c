a895f31548d8e25401ab36ea9361bc01f7c8c089
gusgalvism <gusgalvism@gmail.com>
2020-02-11 03:21:25 +0000

    Task 6,7,8

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+		return (a * -1);
+	else
+		return (a);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+	int a;
+
+	if (b >= 0)
+		a = b % 10;
+	else
+		a = (b % 10) * -1;
+	_putchar(a + '0');
+	return (a);
+}
