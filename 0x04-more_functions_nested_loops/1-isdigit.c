8d28ac5bf4ed093d407459d832f0dd7fd8396110
gusgalvism <gusgalvism@gmail.com>
2020-02-13 16:53:09 +0000

    Task 1,2,3

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _isdigit - checks for a digit (0 through 9).
+ * @c: int type
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		Return(1);
+	}
+	Return(0);
+}
+
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two integers.
+ * @a: int type
+ * @b: int type
+ * Return: mul
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
