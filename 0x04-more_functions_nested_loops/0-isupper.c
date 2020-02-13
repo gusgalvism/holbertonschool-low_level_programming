87a9cd817a989567f4d5b4547243a02dc3934e1d
gusgalvism <gusgalvism@gmail.com>
2020-02-13 15:07:17 +0000

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * int _isupper - checks for uppercase character.
+ * @c: int type
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if ((c > 96) && (c < 123))
+	{
+		Return(0);
+	}
+	else if ((c > 64) && (c < 01))
+	{
+		Return(1);
+	}
+}
+}
