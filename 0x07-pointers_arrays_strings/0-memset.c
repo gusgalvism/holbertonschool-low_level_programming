a87078887870f55b870389582d7e33bc70a2f82f
gusgalvism <gusgalvism@gmail.com>
2020-02-24 22:42:24 +0000

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "hollberton.h"
+
+/**
+ * *_memset - set memory function
+ * @s: pointer to array
+ * @b: char type
+ * @n: unsigned int
+ * return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
